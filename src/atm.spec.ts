e685500a1317010536bbe32ca919f3e4b2e6acfb
moreirathomas <moreirathomas97@gmail.com>
2021-04-29 12:10:06 +0200

    ⚡ Improve useAtm func: receives a number as input

--- src/atm.spec.ts
+++ src/atm.spec.ts
@@ -1,18 +1,24 @@
-import { useAtm } from "./atm"
+import { fmtBills, useAtm } from "./atm"
 
 describe("atm", function () {
-  it("should format correctly the result string", function () {
-    expect(useAtm([0, 0, 0, 0, 0, 1])).toEqual("1 10€")
-    expect(useAtm([1, 1, 1, 1, 1, 1])).toEqual("1 500€, 1 200€, 1 100€, 1 50€, 1 20€, 1 10€")
-    expect(useAtm([1, 2, 3, 4, 5, 6])).toEqual("1 500€, 2 200€, 3 100€, 4 50€, 5 20€, 6 10€")
+  it("should return a tuple for a valid amount input", function () {
+    expect(useAtm(10)).toEqual([0, 0, 0, 0, 0, 1])
+    expect(useAtm(100)).toEqual([0, 0, 1, 0, 0, 0])
+    expect(useAtm(490)).toEqual([0, 2, 0, 1, 2, 0])
   })
 
-  it("should handle a null value", function () {
+  it("should format the result into a string", function () {
+    expect(fmtBills([0, 0, 0, 0, 0, 1])).toEqual("1 10€")
+    expect(fmtBills([1, 1, 1, 1, 1, 1])).toEqual("1 500€, 1 200€, 1 100€, 1 50€, 1 20€, 1 10€")
+    expect(fmtBills([1, 2, 3, 4, 5, 6])).toEqual("1 500€, 2 200€, 3 100€, 4 50€, 5 20€, 6 10€")
+  })
+
+  it("should handle a null value formatting", function () {
     // Silence the ts compiler warning.
-    expect(useAtm([] as any)).toEqual("")
+    expect(fmtBills([] as any)).toEqual("")
   })
 
   it("should handle non-first zeros formatting", function () {
-    expect(useAtm([0, 1, 0, 1, 0, 1])).toEqual("1 200€, 1 50€, 1 10€")
+    expect(fmtBills([0, 1, 0, 1, 0, 1])).toEqual("1 200€, 1 50€, 1 10€")
   })
 })
--- src/atm.ts
+++ src/atm.ts
@@ -1,25 +1,33 @@
 import { Tuple } from "./type"
 
-// This map seems better than a switch statement inside the function body.
-const indexToAmount: Record<number, number> = {
-  0: 500,
-  1: 200,
-  2: 100,
-  3: 50,
-  4: 20,
-  5: 10,
+type BillsTuple = Tuple<number, 6>
+
+const availableBills = [500, 200, 100, 50, 20, 10] as const
+
+type ReduceAccumulator = { rest: number; bills: number[] }
+
+export function useAtm(amount: number): BillsTuple {
+  const res = availableBills.reduce(
+    (acc: ReduceAccumulator, currBill) => {
+      // Amount that cannot fit in any number of the current bill.
+      const rest = acc.rest % currBill
+      // Quantity of current bill to sum up to the amount that can fit.
+      const currBillQty = (acc.rest - rest) / currBill
+      return { rest, bills: [...acc.bills, currBillQty] }
+    },
+    { rest: amount, bills: [] }
+  )
+  return res.bills as BillsTuple
 }
 
-// Tuple of 6 numbers.
-
-export function useAtm(input: Tuple<number, 6>): string {
+export function fmtBills(input: BillsTuple): string {
   let str: string = ""
   input.forEach((qty, i) => {
     if (str.length && qty > 0) {
       str += ", "
     }
     if (qty > 0) {
-      str += `${qty} ${indexToAmount[i]}€`
+      str += `${qty} ${availableBills[i]}€`
     }
   })
 
